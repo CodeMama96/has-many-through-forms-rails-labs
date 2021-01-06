ab97e6fcef2e18948663ca078d024e93f5765c19
Alicia Weenum <weenum.alicia@gmail.com>
2021-01-06 17:02:21 -0500

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,28 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @categories = Category.all
   end
+
+  def create
+    @category = Category.find(params[:id])
+    @category.post = Post.find_or_create_by(post_params)
+
+    if @category.save
+      redirect_to @category
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def category_params
+    params.require(:category).premit(:name)
+  end
+
+  def post_params
+    params.require(:post).permit(:title)
+  end
+
 end
